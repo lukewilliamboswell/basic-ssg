18b31bd96f1495fa6fd1d6a706f2c8b4c9b71c95
Luke Boswell <lukewilliamboswell@gmail.com>
2024-04-24 15:38:22 +1000

    fixup

--- run.sh
+++ run.sh
@@ -15,8 +15,8 @@ case "$OS" in
                 cp target/aarch64-apple-darwin/debug/libhost.a platform/macos-arm64.a
                 ;;
             "x86_64")
-                cargo build --release --target=x86_64-apple-darwin
-                cp target/aarch64-apple-darwin/release/libhost.a platform/macos-x64.a
+                cargo build --target=x86_64-apple-darwin
+                cp target/aarch64-apple-darwin/debug/libhost.a platform/macos-x64.a
                 ;;
             *)
                 echo "Unsupported architecture: $ARCH"
@@ -27,12 +27,12 @@ case "$OS" in
     "Linux")
         case "$ARCH" in
             "x86_64")
-                cargo build --release --target=x86_64-unknown-linux-musl
-                cp target/x86_64-unknown-linux-musl/release/libhost.a platform/linux-x64.a
+                cargo build --target=x86_64-unknown-linux-musl
+                cp target/x86_64-unknown-linux-musl/debug/libhost.a platform/linux-x64.a
                 ;;
             "aarch64")
-                cargo build --release --target=aarch64-unknown-linux-musl
-                cp target/aarch64-unknown-linux-musl/release/libhost.a platform/linux-arm64.a
+                cargo build --target=aarch64-unknown-linux-musl
+                cp target/aarch64-unknown-linux-musl/debug/libhost.a platform/linux-arm64.a
                 ;;
             *)
                 echo "Unsupported architecture: $ARCH"
