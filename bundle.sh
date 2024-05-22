6f1d1cf03d041f0ecd932955905ac8888518728a
Luke Boswell <lukewilliamboswell@gmail.com>
2024-05-22 11:40:05 +1000

    change to gnu (rust musl is broken on linux)

--- bundle.sh
+++ bundle.sh
@@ -11,19 +11,19 @@ rm -f platform/macos-x64.a
 
 # ADD TARGETS
 rustup target add aarch64-apple-darwin
-rustup target add x86_64-unknown-linux-musl
+rustup target add x86_64-unknown-linux-gnu
 rustup target add x86_64-apple-darwin
-rustup target add aarch64-unknown-linux-musl
+rustup target add aarch64-unknown-linux-gnu
 
 # LEGACY LINKER ARTEFACTS
 cargo build --release --target=aarch64-apple-darwin
 cp target/aarch64-apple-darwin/release/libhost.a platform/macos-arm64.a
 
-cargo build --release --target=aarch64-unknown-linux-musl
-cp target/aarch64-unknown-linux-musl/release/libhost.a platform/linux-arm64.a
+cargo build --release --target=aarch64-unknown-linux-gnu
+cp target/aarch64-unknown-linux-gnu/release/libhost.a platform/linux-arm64.a
 
-cargo build --release --target=x86_64-unknown-linux-musl
-cp target/x86_64-unknown-linux-musl/release/libhost.a platform/linux-x64.a
+cargo build --release --target=x86_64-unknown-linux-gnu
+cp target/x86_64-unknown-linux-gnu/release/libhost.a platform/linux-x64.a
 
 cargo build --release --target=x86_64-apple-darwin
 cp target/aarch64-apple-darwin/release/libhost.a platform/macos-x64.a
